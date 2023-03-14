033a9b302d28f66b0f7727c0653a55fd7ced9c74
GabriTom <gabriele.tomatis99@gmail.com>
2023-03-14 10:53:28 +0100

    Es nuovi, ma vecchi, reupdate perchè os161 ha rotto tutto

---
+++ Esercizi/es0314.js
@@ -0,0 +1,30 @@
+"use strict";
+const sqlite = require('sqlite');
+
+//Asynchronous programming
+const sussi=function(){
+    console.log("AAAAAAAAAAAAAAAAAAAAAses");
+}
+setTimeout(sussi, 2000);
+
+//SQLite programming
+const db = new sqlite.Database('exams.sqlit', (err) => { if (err) throw err; });
+let result = [];
+let sql = "SELECT * FROM course LEFT JOIN score ON course.code=score.coursecode" ;
+
+db.all(sql, (err,rows)=>{
+    if(err) throw err ;
+    for (let row of rows) {
+        console.log(row);
+        result.push(row);
+    }
+});
+/*
+Se qua non aspettassi lui va avanti, quindi mi stampa
+******** e non la risposta del server perchè essa è gestita in maniera asincrona
+*/
+console.log('*************');
+for (let row of result) {
+    console.log(row);
+}
+console.log('****** END OF DB ******');
